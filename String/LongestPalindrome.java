f623afda2570f6b8d9df3839f3aa35bc607ac884
unknown <kiranjeetsingh1773@gmail.com>
2026-01-20 08:31:55 +0530

    longest palindrome

---
+++ String/LongestPalindrome.java
@@ -0,0 +1,47 @@
+package String;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        if(s.length()<=1){
+            return s;
+        }
+
+        String LPS = "";
+
+        for( int i =1; i< s.length();i++){
+
+            int low = i;
+            int high = i;
+
+            while(low >=0 && high < s.length()
+                    && s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+
+
+            }
+
+            String palindrome = s.substring(low+1,high);
+            if(palindrome.length() > LPS.length()){
+                LPS = palindrome;
+            }
+
+            low = i-1;
+            high = i;
+
+            while(low >= 0 && high < s.length()
+                    && s.charAt(low ) == s.charAt(high)){
+                low--;
+                high++;
+
+            }
+
+            palindrome = s.substring(low+1,high);
+            if(palindrome.length() > LPS.length()){
+                LPS = palindrome;
+            }
+        }
+        return LPS;
+    }
+
+}
